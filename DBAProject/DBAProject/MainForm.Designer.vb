88f79bcf7384c314fdbc3da13d05f7e400ce1ac0
Andrew <Spyder045@gmail.com>
2012-05-04 16:07:34 -0400

    Intial Build

---
+++ DBAProject/DBAProject/MainForm.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblDateOut = New System.Windows.Forms.Label()
+        Me.lblPassOut = New System.Windows.Forms.Label()
+        Me.lblNameOut = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(49, 212)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(238, 212)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "First Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Last Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(81, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Todays Date:"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(78, 23)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(133, 20)
+        Me.txtFirstName.TabIndex = 6
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(78, 49)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(133, 20)
+        Me.txtLastName.TabIndex = 7
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(74, 109)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(133, 20)
+        Me.txtPassword.TabIndex = 8
+        '
+        'lblDateOut
+        '
+        Me.lblDateOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDateOut.Location = New System.Drawing.Point(158, 173)
+        Me.lblDateOut.Name = "lblDateOut"
+        Me.lblDateOut.Size = New System.Drawing.Size(133, 24)
+        Me.lblDateOut.TabIndex = 9
+        '
+        'lblPassOut
+        '
+        Me.lblPassOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPassOut.Location = New System.Drawing.Point(238, 109)
+        Me.lblPassOut.Name = "lblPassOut"
+        Me.lblPassOut.Size = New System.Drawing.Size(133, 20)
+        Me.lblPassOut.TabIndex = 10
+        '
+        'lblNameOut
+        '
+        Me.lblNameOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNameOut.Location = New System.Drawing.Point(238, 26)
+        Me.lblNameOut.Name = "lblNameOut"
+        Me.lblNameOut.Size = New System.Drawing.Size(133, 24)
+        Me.lblNameOut.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 261)
+        Me.Controls.Add(Me.lblNameOut)
+        Me.Controls.Add(Me.lblPassOut)
+        Me.Controls.Add(Me.lblDateOut)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form1"
+        Me.Text = "Awesome String Manipulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents lblDateOut As System.Windows.Forms.Label
+    Friend WithEvents lblPassOut As System.Windows.Forms.Label
+    Friend WithEvents lblNameOut As System.Windows.Forms.Label
+
+End Class
---
+++ DBAProject/DBAProject/MainForm.vb
@@ -0,0 +1,78 @@
+﻿'Andrew hall
+Option Strict On
+
+Imports MySql.Data.MySqlClient
+
+
+Public Class MainForm
+
+    Dim MysqlConn As New MySqlConnection()
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        'Close the form
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As System.Object, e As System.EventArgs) Handles btnSubmit.Click
+
+        Dim namecmd As New MySqlCommand
+        Dim dateCmd As New MySqlCommand
+        Dim passCmd As New MySqlCommand
+
+        MysqlConn.ConnectionString = "server= localhost; uid=root; database=finalproject"
+        Try
+            MysqlConn.Open()
+
+            namecmd.Connection = MysqlConn
+            namecmd.CommandText = "stringTogether"
+            namecmd.CommandType = CommandType.StoredProcedure
+            namecmd.Parameters.Add("?in_firstname", txtFirstName.Text)
+            namecmd.Parameters("?in_firstname").DbType = DbType.StringFixedLength
+            namecmd.Parameters("?in_firstname").Direction = ParameterDirection.Input
+            namecmd.Parameters.Add("?in_lastname", txtLastName.Text)
+            namecmd.Parameters("?in_firstname").DbType = DbType.StringFixedLength
+            namecmd.Parameters("?in_firstname").Direction = ParameterDirection.Input
+            namecmd.Parameters.Add("?results", MySqlDbType.String)
+            namecmd.Parameters("?results").Direction = ParameterDirection.Output
+            namecmd.ExecuteNonQuery()
+
+            lblNameOut.Text = namecmd.Parameters("?results").Value.ToString
+
+            dateCmd.Connection = MysqlConn
+            dateCmd.CommandText = "whatTime"
+            dateCmd.CommandType = CommandType.StoredProcedure
+            dateCmd.Parameters.Add("?results", MySqlDbType.String)
+            dateCmd.Parameters("?results").Direction = ParameterDirection.Output
+            dateCmd.ExecuteNonQuery()
+
+            lblDateOut.Text = dateCmd.Parameters("?results").Value.ToString
+
+            passCmd.Connection = MysqlConn
+            passCmd.CommandText = "hashPassword"
+            passCmd.CommandType = CommandType.StoredProcedure
+            passCmd.Parameters.Add("?in_password", txtPassword.Text)
+            passCmd.Parameters("?in_password").Direction = ParameterDirection.Input
+            passCmd.Parameters("?in_password").DbType = DbType.StringFixedLength
+            passCmd.Parameters.Add("?results", MySqlDbType.String)
+            passCmd.Parameters("?results").Direction = ParameterDirection.Output
+            passCmd.ExecuteNonQuery()
+
+            lblPassOut.Text = passCmd.Parameters("?results").Value.ToString
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Number & " " & ex.Message)
+        End Try
+
+        MysqlConn.Close()
+    End Sub
+End Class
